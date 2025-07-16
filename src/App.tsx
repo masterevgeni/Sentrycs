ba5f39e8040b5f34b07ec752ae05402d2a0e4e6c
Evgeni <souldata2000@yahoo.com>
2025-07-16 14:11:30 +0300

    init react project

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Main from './pages/Main/Main';
+
+import './App.css';
+
+const App: React.FC = () => {
+  return (
+    <Main />
+  );
+};
+
+export default App;
---
+++ src/pages/Main/Main.tsx
@@ -0,0 +1,8 @@
+function Main() {
+  return (
+    <div className="container">
+  <h2>Welcome to the Main Page</h2>
+    </div>
+  );
+}
+export default Main;
